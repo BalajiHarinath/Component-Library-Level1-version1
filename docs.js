ed4731020eee6701b8925085de655e018c04e608
BalajiHarinath <89400019+BalajiHarinath@users.noreply.github.com>
2022-02-18 22:29:22 +0530

    fix : fixed the responsive issues for mobile display

--- docs.js
+++ docs.js
@@ -1,6 +1,7 @@
 const sidebarButton = document.querySelector(".sidebar-arrow");
 const sidebarLinks = document.querySelectorAll(".sidebar-list-item");
 const sidebarDisplay = document.querySelector(".sidebar-display");
+const sidebarMobileDisplay = document.querySelector(".sidebar-mobile-display");
 const documentDisplay = document.querySelector(".main");
 const footer = document.querySelector(".footer");
 
@@ -11,6 +12,7 @@ arrayOfClickableItems.forEach((button)=>{
         sidebarDisplay.classList.toggle("sidebar-active");
         documentDisplay.classList.toggle("main-hide");
         sidebarButton.classList.toggle("sidebar-arrow-rotate");
+        sidebarMobileDisplay.classList.toggle("sidebar-mobile-display-open")
         footer.classList.toggle("footer-hide");
     })
 })
